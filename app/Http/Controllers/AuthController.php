303c736576d829ee311a0275ac8d6ceb6a9ddc1a
Shakyakithmini <shakyakhasangana@gmail.com>
2024-10-12 09:41:11 -0500

    admin&Dashboard

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
-
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
 
 
 
@@ -21,6 +24,35 @@ Validator::make($request->all(),[
     'password'=>'required|confirmed'
 ])->validate();
 
+User ::create([
+    'name'=>$request->name,
+    'email'=>$request->email,
+    'password'=>Hash::make($request->password),
+    'level'=>'Admin'
+]);
+return redirect()->route('login');
+}
+public function login()
+{
+    return view('auth/login');
+}
+public function loginAction(Request $request)
+{
+    Validator::make($request->all(),[
+'email'=>'required|email',
+'password'=>'required'
+    ])->validate();
+
+    if (!Auth::attempt($request->only('email', 'password'), $request->boolean('remember'))) {
+        throw ValidationException::withMessages([
+            'email' => trans('auth.failed')
+        ]);
+    }
+
+    $request->session()->regenerate();
+
+    return redirect()->route('dashboard');
+}
 }
 
-}
+
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+  
+@section('title', 'Admin Panel With Login and Registration')
+  
+@section('contents')
+  <div class="row">
+    Dashboard
+  </div>
+@endsection
